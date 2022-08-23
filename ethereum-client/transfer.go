4a7ea2d9032beaecce8f03276fa778063bd44263
Spark.liu <spark.liu@cn.ibm.com>
2022-08-23 09:44:01 +0800

    add

--- ethereum-client/transfer.go
+++ ethereum-client/transfer.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"math/big"
 
@@ -14,93 +15,116 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 	resty "github.com/go-resty/resty/v2"
 	log "github.com/sirupsen/logrus"
+	"github.com/vrischmann/envconfig"
 )
 
-type Resp struct {
-	Uuid      string `json:"uuid"`
-	Action    string `json:"action"`
-	Signature string `json:"signature"`
-}
-
 func main() {
-	client, err := ethclient.Dial("https://eth-rinkeby.alchemyapi.io/v2/··")
+	config, err := loadConfigFromEnv()
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to load config.")
 	}
-	pubstring := "0x048b957dd7eca286ff51a0ad4d61148b814d1f40836d757eb594c4303fda7683d84ce9b879a2f8387f7f9f823a011f86f40ef2e3b88bdb5848e81d17a9810cb8ab"
-	pubByte, err := hexutil.Decode(pubstring)
+	log.WithField("config", config).Info("load config success.")
+
+	// Create a rest Client to call signing server
+	restClient := resty.New()
+
+	// get public key and address
+	getPublicKeyResponseBody := &struct {
+		EthereumPublicKey string
+		Address           string
+	}{}
+
+	getPublicKeyendpoint := fmt.Sprintf("http://%s:%s:/v1/grep11/key/secp256k1/get_ethereum_key/%s", config.SigningServerAddress, config.SigningServerPort, config.KeyUUID)
+	log.WithField("endpoint", getPublicKeyendpoint).Info("start call signing server to get public address")
+
+	_, err = restClient.R().EnableTrace().SetResult(getPublicKeyResponseBody).Get(getPublicKeyendpoint)
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to get public key from signing server")
+	}
+	log.WithField("from_address", getPublicKeyResponseBody.Address).WithField("public key", getPublicKeyResponseBody.Address).Info("success get public key")
+
+	// connect to ethereum
+	client, err := ethclient.Dial(config.EthClient)
+	if err != nil {
+		log.WithField("ethereum endpoint", config.EthClient).WithError(err).Fatal("fail to connect ethereum")
+	}
+
+	pubString := getPublicKeyResponseBody.EthereumPublicKey
+	pubByte, err := hexutil.Decode(pubString)
+	if err != nil {
+		log.WithError(err).Fatal("fail to decode public key")
 	}
 
 	publickey, err := crypto.UnmarshalPubkey(pubByte)
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to unmarshal public key")
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publickey)
-	fmt.Println(fromAddress.Hex()) // 0x96216849c49358B10257cb55b28eA603c874b05E
+	log.WithField("from address", fromAddress).Info("extract from address from public key")
+
 	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("failed to get nonce")
 	}
-	fmt.Println(nonce)
+	log.WithField("nonce", nonce).Info("success get nonce")
 
-	value := big.NewInt(1000000000000000) // in wei (0.001 eth)
-	gasLimit := uint64(21000)             // in units
+	value := big.NewInt(int64(config.Value * 1000000000000000000)) // in wei (0.001 eth)
+	gasLimit := uint64(21000)                                      // in units
 	gasPrice, err := client.SuggestGasPrice(context.Background())
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to get gas price")
 	}
-	toAddress := common.HexToAddress("0xd7c6b20Aa8a7f42cca2a945144426546010eD9C3")
+	toAddress := common.HexToAddress(config.ToAddress)
 	var data []byte
+	log.WithField("nonce", nonce).WithField("to_address", toAddress).WithField("value", value).WithField("gas_price", gasPrice).WithField("gas_limit", gasLimit).Info("start a new transaction")
 	tx := types.NewTransaction(nonce, toAddress, value, gasLimit, gasPrice, data)
+
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to get chain ID")
 	}
 	log.WithField("chain_id", chainID).Info("chain_id")
+
 	signer := types.NewEIP155Signer(chainID)
 	hs := signer.Hash(tx)
-	// Create a Resty Client
-	restClient := resty.New()
-	respbody := &Resp{}
-	sig := []byte{}
-	_, err = restClient.R().
-		EnableTrace().
-		SetResult(respbody).
-		SetBody(map[string]interface{}{"data": toString(hs.Bytes())}).
-		Post("http://localhost:8080:/v1/grep11/key/secp256k1/sign/10b2f9c0-fdc3-402e-a58d-d9931b8313dc")
+
+	// sign by signing server
+	sigResponse := &struct {
+		Uuid      string `json:"uuid"`
+		Action    string `json:"action"`
+		Signature string `json:"signature"`
+	}{}
+
+	signEndpoint := fmt.Sprintf("http://%s:%s:/v1/grep11/key/secp256k1/sign/%s", config.SigningServerAddress, config.SigningServerPort, config.KeyUUID)
+	log.WithField("endpoint", getPublicKeyendpoint).Info("start call signing server to sign")
+	_, err = restClient.R().EnableTrace().SetResult(sigResponse).SetBody(map[string]interface{}{"data": toString(hs.Bytes())}).Post(signEndpoint)
 	if err != nil {
-		log.WithError(err).Error("fail to call HPCS")
-		panic(err)
+		log.WithError(err).Fatal("fail to call signing server to sign")
 	}
-	sig = toByte(respbody.Signature)
+
+	sig := toByte(sigResponse.Signature)
 	r := new(big.Int).SetBytes(sig[:32])
 	s := new(big.Int).SetBytes(sig[32:64])
 
 	sig2 := LocalChange(r, s)
-	log.WithField("pub_len", len(pubByte)).WithField("len", len(sig)).WithField("sig", respbody.Signature).Info("sign by HPCS")
+	log.WithField("public_key_len", len(pubByte)).WithField("signature_len", len(sig2)).WithField("signature", toString(sig2)).Info("sign by HPCS")
 
-	result123 := crypto.VerifySignature(pubByte, hs.Bytes(), sig2)
-	log.WithField("result", result123).Info("resut")
+	signatureNoRecoverID := sig2[:len(sig2)-1] // remove recovery id
+	localVerifyResult := crypto.VerifySignature(pubByte, hs.Bytes(), signatureNoRecoverID)
+	log.WithField("result", localVerifyResult).Info("local verify result")
 
-	sig = append(sig, 0)
-
-	fmt.Println(len(sig))
-
-	signedTx, err := tx.WithSignature(signer, sig)
+	signedTx, err := tx.WithSignature(signer, sig2)
 	if err != nil {
-		log.WithError(err).Error("failed to sign ")
-		panic(err)
+		log.WithError(err).Fatal("failed to sign ")
 	}
 
 	err = client.SendTransaction(context.Background(), signedTx)
 	if err != nil {
-		panic(err)
+		log.WithError(err).Fatal("fail to broadcast to ethereum")
 	}
-
-	fmt.Printf("tx sent: %s \n", signedTx.Hash().Hex())
+	fmt.Println("Pls check result with following link:")
+	fmt.Printf("		https://rinkeby.etherscan.io/tx/%s \n", signedTx.Hash().Hex())
 }
 
 func toString(src []byte) string {
@@ -141,4 +165,27 @@ func LocalChange(R, S *big.Int) []byte {
 		//	log.Println("Else Result : ", verifyECRecover(hash.Bytes(), sig, expectedPubKey))
 	}
 	return sig
+}
+
+type Config struct {
+	EthClient              string
+	KeyUUID                string
+	ToAddress              string
+	SigningServerAddress   string
+	SigningServerPort      string
+	SIGNING_SERVER_ADDRESS string
+	Value                  float32
+}
+
+func (c *Config) String() string {
+	v, _ := json.Marshal(c)
+	return string(v)
+}
+
+func loadConfigFromEnv() (*Config, error) {
+	config := &Config{}
+	if err := envconfig.Init(&config); err != nil {
+		return nil, err
+	}
+	return config, nil
 }
